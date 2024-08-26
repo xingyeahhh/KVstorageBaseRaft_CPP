7b05d06e4239ff13f411b8bfa20a553f1ff3df65
XingYeahhh!!! <123461462+xingyeahhh@users.noreply.github.com>
2024-08-26 01:22:46 +0100

    Update skipList.h

--- src/skipList/include/skipList.h
+++ src/skipList/include/skipList.h
@@ -4,13 +4,7 @@
 
 #ifndef SKIPLIST_H
 #define SKIPLIST_H
-/* ************************************************************************
-> File Name:     skiplist.h
-> Author:        程序员Carl
-> 微信公众号:    代码随想录
-> Created Time:  Sun Dec  2 19:04:26 2018
-> Description:
- ************************************************************************/
+
 
 #include <cmath>
 #include <cstdlib>
